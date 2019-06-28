f01fb1110b553c333f32a9ffac5c59c206963c27
Dmitriy Kovtun <dkovtun.work@gmail.com>
2019-06-28 16:15:18 +0300

    lowdb added

---
+++ db.js
@@ -0,0 +1,10 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const path = require('path');
+
+const adapter = new FileSync(path.resolve(__dirname, './db.json'));
+const db = low(adapter);
+
+db.defaults({ results: [] }).write();
+
+module.exports = db;
