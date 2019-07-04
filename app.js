c1767e405e5091dd1e7b57dcdf762d26a28010f7
Dmitriy Kovtun <dkovtun.work@gmail.com>
2019-07-04 12:20:43 +0300

    disable error page rendering

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -32,9 +32,7 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.sendStatus(err.status || 500);
 });
 
 module.exports = app;
